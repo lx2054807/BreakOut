bed424cdd5be990cb80c5492a0f41728b6fe5a4c
lixin <lx199317@foxmail.com>
2020-07-31 19:08:42 +0800

    remove redundancy

--- game.cpp
+++
@@ -1,9 +0,0 @@
-#include "game.h"
-
-Game::Game(unsigned int height, unsigned int width) {}
-Game::~Game(){}										  
-void Game::Init(){}									  
-void Game::ProcessInput(){}							  
-void Game::Update(){}								  
-void Game::Render(){}								  
-														  
